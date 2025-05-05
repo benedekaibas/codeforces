b4b3fea13b46f96899410e219305dc60eaed9a0a
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-05 15:13:24 -0500

    finished first problem

---
+++ src/watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ranges>
+using namespace std;
+
+int division(int num, int weight){
+  if (weight % num == 0){
+    return num;
+  } else {
+    return false;
+  }
+}
+
+int main(){
+  int result = division(2, 80);
+  cout << result;
+}
