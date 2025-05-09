c3fcf85f86218db2415b4f4baa6f183c7eb23f8a
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-09 09:09:01 -0500

    progress with second function still error message

--- src/way_2_long_words.cpp
+++ src/way_2_long_words.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 
+string return_str(string input_text){
+  return input_text;
+}
 
+char store_first_element(){
+  string text = return_str;
 
+  for(char e : text){
+    return text[0];
+  }
+}
 
 int main(){
-  return 0;
+  string text = "Hey";
+  string output = return_str(text);
+  cout << output;
 }
