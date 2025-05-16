bd26bf2fe8240a4bbedc5f1cf73f494074ec91da
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-16 22:45:20 +0200

    passed ptr to function for avoiding buffer overflow

--- src/way_2_long_words.cpp
+++ src/way_2_long_words.cpp
@@ -2,21 +2,33 @@
 using namespace std;
 
 
-string return_str(string input_text){
-  return input_text;
-}
-
-char return_first_char(){
+string return_str(){
   string text;
   cout << "Enter a text here: ";
   cin >> text;
 
-  string res = return_str(text);
+  return text;
+}
 
+char return_first_char(){
+
+  string res = return_str();
   return res[0];
 }
 
+string return_last_char(){
+  string res = return_str();
+
+  char last_char = res.back();
+
+  char* p = &last_char;
+  return res.erase(*p);
+}
+
 int main(){
-  char result = return_first_char();
-  cout << "First char is: " << result << endl;
+  char first = return_first_char();
+  cout << "First char is: " << first << endl;
+
+  string last = return_last_char();
+  cout << "Last char is: " << last << endl;
 }
