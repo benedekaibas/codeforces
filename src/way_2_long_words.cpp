d90224e7d2bd29f03b6d0fab1c4ede40500ace30
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-27 12:34:16 +0200

    correct implementation of returning strings are implemented

--- src/way_2_long_words.cpp
+++ src/way_2_long_words.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
-using namespace std;
+#include <utility>
+#include <string>
 
 
-string return_str(string name){
+std::string name(){
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+
   return name;
 }
 
-void rm_chars(string (*func)(string)){
-  string name;
-  string snd_name = "Benedek";
-  cout << func(name) << endl; 
-  cout << snd_name << endl;
+std::string rm_first_char(std::string remove){
+  std::string* ptr = &remove;
+  ptr->erase(1);
+
+  return *ptr;
 }
 
+std::pair<std::string, std::string> return_original_modified(std::string &rm_char){
+  std::string modified = rm_char;
+  modified.erase(1);
+  //these two lines are only for checking if the function works well
+  std::cout << rm_char << std::endl;
+  std::cout << modified << std::endl;
+  
+  return{rm_char, modified};
+}
+
+
+
 int main(){
-  string name;
-  cout << "Enter your name: ";
-  cin >> name;
+  std::string usr_name = name();
+  std::pair<std::string, std::string> remove_name = return_original_modified(usr_name);
 
-  return_str(name);
-
-  rm_chars(return_str);
+  //std::cout << remove_name << std::endl;
 }
